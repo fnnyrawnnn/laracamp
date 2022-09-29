ac0e3a029d4317bc0535c5cdef4639290298ca72
fnnyrawnnn <fannyirawannn@gmail.com>
2022-09-29 14:24:26 +0700

    update seeder

---
+++ database/seeders/CampTableSeeder.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Camp;
+
+class CampTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $camps = [
+            [
+                'title' => 'Gila Belajar',
+                'slug' => 'gila-belajar',
+                'price' => 280,
+                'created_at' => date('Y-m-d H:i:s', time()),
+                'updated_at' => date('Y-m-d H:i:s', time()),
+            ],
+            [
+                'title' => 'Baru Mulai',
+                'slug' => 'baru-mulai',
+                'price' => 140,
+                'created_at' => date('Y-m-d H:i:s', time()),
+                'updated_at' => date('Y-m-d H:i:s', time()),
+            ],
+        ];
+
+        //1st method tidak perlu menambahkan created_at & updated_at karena akan otomatis ditambahkan
+        // foreach ($camps as $key => $camp){
+        //     Camp::create($camp);
+        // }
+
+        //2nd method perlu menambahkan data created_at & updated_at karena jika tidak, data created_at & updated_at akan bernilai null
+        Camp::insert($camps);
+    }
+}
