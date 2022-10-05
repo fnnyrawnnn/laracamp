bf171a0cb15d7a2ad1af51d7f8f2515e5d6ef863
fnnyrawnnn <fannyirawannn@gmail.com>
2022-10-05 18:33:13 +0700

    install and integration socialite

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -15,11 +15,11 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600&display=swap" rel="stylesheet">
 
-    <title>Laracamp by BuildWith Angga</title>
+    <title>Laracamp Bootcamp</title>
 </head>
 
 <body>
-    @include('component.navbar')
+    @include('components.navbar')
 
     @yield('content')
 
